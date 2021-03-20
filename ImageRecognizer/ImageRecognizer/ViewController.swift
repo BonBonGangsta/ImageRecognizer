8f5542aaf08128019e52920ab99f7658cc3e7878
Byron De Paz <bl3203@gmail.com>
2021-03-20 11:57:35 -0400

    I forgot what I did to be honest

--- ImageRecognizer/ImageRecognizer/ViewController.swift
+++ ImageRecognizer/ImageRecognizer/ViewController.swift
@@ -11,6 +11,7 @@ import Vision // used to applu classification models to images
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate , UINavigationControllerDelegate {
     var resnetModel = Resnet50()
+    var chapter7Model = Chapter7Attempt3()
     var imagePicker = UIImagePickerController()
     
     @IBOutlet weak var navBar: UINavigationItem!
@@ -35,7 +36,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate , UINavi
     }
     
     func classifyPicture(image: UIImage){
-        // get a model reference with VNCoreML and providing resNetModel as the argument
+         // get a model reference with VNCoreML and providing resNetModel as the argument
         // VNCoreMLModel is the container for the Core ML model used in Vision
         if let model = try? VNCoreMLModel (for: resnetModel.model){
             // the model reference will create an analysis request for the image and processed through
@@ -53,6 +54,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate , UINavi
                 try? handler.perform([request])
             }
         }
+        
+        /*
+        // this should attempt to use my ML model to check the animals tested. it does not assign the label to
+        // the classLabel
+        if let model = try? VNCoreMLModel (for: chapter7Model.model){
+            // the model reference will create an analysis request for the image and processed through
+            // the handler
+            let request = VNCoreMLRequest(model: model) {(request,error) in
+                if let results = request.results as? [VNClassificationObservation]{
+                    //print(results)
+                    let result = results[0]
+                    self.navBar?.title = result.identifier
+                }
+            }
+            // The completion handler is called when we get back information after the request has been
+            // completed. then we execute it with the perform method of VNImageRequestHandler
+            if let imageData = image.jpegData(compressionQuality: 1.0){
+                let handler = VNImageRequestHandler(data:imageData, options:[:])
+                try? handler.perform([request])
+            }
+        } */
     }
     
     func imagePickerController(_ picker:UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
